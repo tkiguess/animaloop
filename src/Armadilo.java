2d5e260d76e18cbe27ab49f43150b3b74e643c80
GBTC441004UR <GBTC441004UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-25 09:33:17 -0400

    Initial commit

---
+++ src/Armadilo.java
@@ -0,0 +1,14 @@
+public class Armadilo extends Animal {
+
+    public Armadilo (){
+
+        System.out.println("Now I am an animal that broke out of the pokemon universe, and made it into the real world.");
+
+    }
+
+    @Override
+    public String sleep(){
+
+    }
+
+}
---
+++ src/Cat.java
@@ -0,0 +1,22 @@
+public class Cat extends Animal {
+
+    public Cat() {
+       //  super();
+        System.out.println("Now I am a Cat!");
+    }
+
+    @Override
+    public String sleep() {
+        return "A cat sleeps...";
+    }
+
+    @Override
+    public String eat() {
+        return "A cat eats...";
+    }
+
+    public String purr() {
+        return "purrr...";
+    }
+
+}
